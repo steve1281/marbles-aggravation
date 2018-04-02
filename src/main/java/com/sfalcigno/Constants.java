f680a849770e34b1df4a691a7fe566ca6cd77c36
Steve Falcigno <steve.falcigno@oracle.com>
2018-04-02 07:35:30 -0400

    working on protability

--- src/main/java/com/sfalcigno/Constants.java
+++ src/main/java/com/sfalcigno/Constants.java
@@ -2,8 +2,9 @@ package com.sfalcigno;
 
 public final class Constants {
     private Constants() { }
+    public static final String ROOT="/Users/sfalcign/projects/home-github/marbles-aggravation";
 
-    public static final String JSON_FOLDER = "/home/steve/projects/marbles-aggravation/json/";
+    public static final String JSON_FOLDER = ROOT+"/json/";
     public static final Integer PORT = 9998;
-    public static final String HTML_ROOT = "/home/steve/projects/marbles-aggravation/html";
+    public static final String HTML_ROOT = ROOT+"/html";
 }
