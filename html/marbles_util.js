1fe31237c4b8989fb54afd7297b3f0c1b2302587
Steve Falcigno <steve1281@hotmail.com>
2018-04-10 07:45:13 -0400

    added contains method

--- html/marbles_util.js
+++ html/marbles_util.js
@@ -38,3 +38,31 @@ function game_state_toString()
 
 }
 
+// https://stackoverflow.com/questions/1181575/determine-whether-an-array-contains-a-value                                                       
+var contains = function(needle) {
+    // Per spec, the way to identify NaN is that it is not equal to itself
+    var findNaN = needle !== needle;
+    var indexOf;
+
+    if(!findNaN && typeof Array.prototype.indexOf === 'function') {
+        indexOf = Array.prototype.indexOf;
+    } else {
+        indexOf = function(needle) {
+            var i = -1, index = -1;
+
+            for(i = 0; i < this.length; i++) {
+                var item = this[i];
+
+                if((findNaN && item !== item) || item === needle) {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        };
+    }
+
+    return indexOf.call(this, needle) > -1;
+};
+
