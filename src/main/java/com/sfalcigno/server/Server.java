0bfa19effc145f4be0b4c7fca1afee7680c538f1
Steve Falcigno <steve1281@hotmail.com>
2018-04-16 19:30:49 -0400

    modified server to run forever

--- src/main/java/com/sfalcigno/server/Server.java
+++ src/main/java/com/sfalcigno/server/Server.java
@@ -15,11 +15,23 @@ public class Server {
     {
         URI baseUri = UriBuilder.fromUri("http://0.0.0.0/").port(port).build();
         ResourceConfig resourceConfig = new ServerApp();
-        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(baseUri, resourceConfig);
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(baseUri, resourceConfig, false);
         // add a static path
         server.getServerConfiguration().addHttpHandler(new StaticHttpHandler(Constants.HTML_ROOT), "/game");
+        try {
+            server.start();
+            Thread.currentThread().join();
+        } catch (Exception ioe) {
+            System.err.println(ioe);
+        } finally {
+                server.stop();
+        }
+        //server.start();
+
+        /*
         System.out.println("press any key to stop server");
         System.in.read();
+        */
     }
 
 }
